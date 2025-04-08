c478dfef46ce5aa348b2322e861eb42265347fc1
Andrew Redd <andrew@redd.org>
2025-04-07 19:35:57 -0600

    at least have something working

--- src/service.js
+++ src/service.js
@@ -6,12 +6,10 @@ const version = require('./version.json');
 const config = require('./config.js');
 const metrics = require('./metrics.js');
 
-
 const app = express();
 app.use(express.json());
 app.use(setAuthUser);
-app.use(metrics.requestTracker);
-app.use('/metrics', metrics.metricsReporter);
+app.use(metrics.requestTracker); 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
